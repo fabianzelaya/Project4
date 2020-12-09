a788b867ca4bffbed80c0ad3420be3560210314b
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2020-12-08 20:55:42 -0800

    Create Proj4_Ex2.cpp
    Hi everyone! This is my first time doing this, but I have a good feeling that this experience is going to bring me good opportunities.
    
    In my files you guys can see the main project which is titled "project4.cpp". I want to use these functions that I wrote in my main project to the Exercise 2(READ ME) - "Proj4_Ex2.cpp" and I was wandering if you guys can look at my project "Proj4_Ex2.cpp" and maybe help me by giving me a second opinion about my code. I want to implement all these functions very well to the output file.
    
    Thank you everyone!

---
+++ Proj4_Ex2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int x = 4, sum = 0;
+    double average;
+
+    ofstream outStream;
+    outStream.open("output");
+
+    outStream << "=== === ============ \n"
+              << " x  x^2 Current Sum. \n"
+              << "=== === ============ \n"
+              << " " << x << "  " << pow(x, 2) << "  " << x * 1 << "\n"
+              << " " << x << "  " << pow(x, 2) << "  " << x * 2 << "\n"
+              << " " << x << "  " << pow(x, 2) << "  " << x * 3 << "\n"
+              << " " << x << "  " << pow(x, 2) << "  " << x * 4 << "\n"
+              << " " << x << "  " << pow(x, 2) << "  " << x * 5 << "\n"
+              << " " << x << "  " << pow(x, 2) << "  " << x * 6 << "\n"
+              << " " << x << "  " << pow(x, 2) << "  " << x * 7 << "\n"
+              << " " << x << "  " << pow(x, 2) << "  " << x * 8 << "\n"
+              << "The average of these 8 numbers is: " << (x * 8 / 8) << endl;
+
+    outStream.close();
+
+    return 0;
+}
+
+// FZ`
